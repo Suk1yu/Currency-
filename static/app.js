a560f36caf22f2b4088c7e5c908cc3079607ee96
F̷a̷r̷&̷̗͙̣̼̣͒́͆͠ <nitroolook@outlook.com>
2025-10-04 17:14:22 +0700

    License In File

--- static/app.js
+++ static/app.js
@@ -1,3 +1,17 @@
+// # Currency Converter Web App
+// # Copyright (C) 2025 far&
+// #
+// # This program is free software: you can redistribute it and/or modify
+// # it under the terms of the GNU General Public License as published by
+// # the Free Software Foundation, either version 3 of the License, or
+// # (at your option) any later version.
+// #
+// # This program is distributed in the hope that it will be useful,
+// # but WITHOUT ANY WARRANTY; See the GNU General Public License for more details.
+// # You should have received a copy of the GNU General Public License
+// # along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 let fromCurrency = "USD";
 let toCurrency = "IDR";
 
